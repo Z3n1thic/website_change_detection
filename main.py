c655f4847a7a3fc9a9eb499bdf07d2ef44a0d68d
z3n1thic <leojud18@htl-kaindorf.at>
2023-01-12 21:29:17 +0100

    renamed get_comp, added write_checks function, documented parts

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 import dotenv
 import hashlib
 import datetime
-import typing
+
 
 real_path = os.path.realpath(os.path.dirname(__file__))
 webhook_url = dotenv.get_key(real_path + "/.env", "WEBHOOK_URL")
@@ -21,7 +21,7 @@ def remove_excessive_newlines(text):
     )  # remove excessive newlines and carriage returns
 
 
-def get_comp(url, html_class="", html_tag="div", html_id=""):
+def get_page_text(url, html_class="", html_tag="body", html_id=""):
     page = urlopen(url)
     html = page.read().decode("utf-8")
     soup = BeautifulSoup(html, "html.parser")
@@ -30,12 +30,14 @@ def get_comp(url, html_class="", html_tag="div", html_id=""):
         args["class_"] = html_class
     if html_id != None and html_id.strip() != "":
         args["id"] = html_id
+    if html_tag == None or html_tag.strip() == "":
+        html_tag = "body"
 
     return remove_excessive_newlines(soup.find(html_tag, **args).get_text())
 
 
 def compare_and_notify(url, html_class, html_tag, name, html_id, checks):
-    out = get_comp(url=url, html_class=html_class, html_tag=html_tag, html_id=html_id)
+    out = get_page_text(url=url, html_class=html_class, html_tag=html_tag, html_id=html_id)
     newhash = hashlib.sha256(out.strip().encode("UTF-8")).hexdigest()
     if newhash != checks[name]["hash"]:
         print("new event")
@@ -47,22 +49,26 @@ def compare_and_notify(url, html_class, html_tag, name, html_id, checks):
         )
         checks[name]["hash"] = newhash
         checks[name]["lastupdated"] = datetime.datetime.now().isoformat()
-        with open(
-            "checks.json", "w"
-        ) as f:  # This not ideal because it results in a lot of disk writes
-            json.dump(checks, f)
+
+    write_checks(checks)
+
+def write_checks(checks):
+    with open("checks.json", "w") as f:
+        json.dump(checks, f, indent=4)
 
 
 if __name__ == "__main__":
     with open(real_path + "/monitor.json") as f:
         monitorlist = json.load(f)
     try:
+        # Handle if checks.json exists or not
         with open("checks.json", "r") as f:
             checks = json.load(f)
     except FileNotFoundError:
+        # Initialize checks.json and exit
         checks = {}
         for monitor in monitorlist:
-            data = get_comp(
+            data = get_page_text(
                 monitor["url"],
                 monitor.get("html_class"),
                 monitor.get("html_tag"),
@@ -72,15 +78,14 @@ if __name__ == "__main__":
                 "hash": hashlib.sha256(data.strip().encode("UTF-8")).hexdigest(),
                 "lastupdated": datetime.datetime.now().isoformat(),
             }
-        with open("checks.json", "w") as f:
-            json.dump(checks, f)
+        write_checks(checks)
         exit()
 
     for monitor in monitorlist:
         # Check if monitor is in checks and add if it isn't
         if monitor["name"] not in checks:
             print(monitor["name"] + "not in checks")
-            data = get_comp(
+            data = get_page_text(
                 monitor["url"],
                 monitor.get("html_class"),
                 monitor.get("html_tag"),
@@ -90,8 +95,7 @@ if __name__ == "__main__":
                 "hash": hashlib.sha256(data.strip().encode("UTF-8")).hexdigest(),
                 "lastupdated": datetime.datetime.now().isoformat(),
             }
-            with open("checks.json", "w") as f:
-                json.dump(checks, f)
+            write_checks(checks)
             continue
 
         compare_and_notify(
